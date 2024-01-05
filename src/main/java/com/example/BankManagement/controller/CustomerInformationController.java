40f17a6aca94d7eb1aab627714125b79cd5ac9be
ashrumochandash <ashrumochandash8@gmail.com>
2024-01-05 13:34:41 +0200

    I have added angular server in controller

--- src/main/java/com/example/BankManagement/controller/CustomerInformationController.java
+++ src/main/java/com/example/BankManagement/controller/CustomerInformationController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.BankManagement.dto.CustomerInformationDTO;
@@ -17,7 +19,9 @@ import com.example.BankManagement.entity.CustomerInformation;
 import com.example.BankManagement.exception.BusinessException;
 import com.example.BankManagement.service.CustomerInformationServiceImpl;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/api/v1")
 public class CustomerInformationController {
 	
 	@Autowired
